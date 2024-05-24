eb99e2b1cd64188077769bb3106a58aa404fbf64
Vaishali <aggarwal.vaishali785@gmail.com>
2024-05-24 18:54:05 +0530

    config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,17 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    root: true,
+    extends: [
+        '@react-native',
+        'standard-with-typescript',
+        'plugin:react/recommended',
+        'prettier',
+        'prettier/react',
+    ],
+    plugins: ['prettier'],
+    rules: {
+        // "prettier/prettier": "error",
+        '@typescript-eslint/indent': ['error', 2],
+        '@typescript-eslint/no-unused-vars': 'error',
+        '@typescript-eslint/no-explicit-any': 'error',
+    },
 };
