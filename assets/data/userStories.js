4a4d566d0e7d74c541f8d81f1a952cf948ecc673
Vaishali <aggarwal.vaishali785@gmail.com>
2024-05-24 18:52:56 +0530

    assets folder- fonts,images and data

---
+++ assets/data/userStories.js
@@ -0,0 +1,50 @@
+const userStories = [
+    {
+        id: 1,
+        firstName: 'Vaishali',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 2,
+        firstName: 'Akash',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 3,
+        firstName: 'Priyanka',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 4,
+        firstName: 'Tapish',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 5,
+        firstName: 'Garvit',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 6,
+        firstName: 'Nisha',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 7,
+        firstName: 'Mohit',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 8,
+        firstName: 'Kuldeep',
+        profileImage: require('../images/default_profile.png'),
+    },
+    {
+        id: 9,
+        firstName: 'Manoj',
+        profileImage: require('../images/default_profile.png'),
+    },
+];
+
+
+export default userStories
