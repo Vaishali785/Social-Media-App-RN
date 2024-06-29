ab89d4e73123aa32aa155cf03cc42945331022d2
Vaishali <aggarwal.vaishali785@gmail.com>
2024-06-29 15:15:49 +0530

    changes to add navigations

--- assets/styles/globalStyle.js
+++ assets/styles/globalStyle.js
@@ -1,44 +1,15 @@
-import { StyleSheet } from "react-native";
-import { getFontFamily } from "../fonts/helper";
+import {StyleSheet} from 'react-native';
 
 const globalStyle = StyleSheet.create({
-    header: {
-        marginLeft: 27,
-        marginRight: 17,
-        marginTop: 30,
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    messageIcon: {
-        padding: 14,
-        backgroundColor: '#f9fafb',
-        borderRadius: 100,
-    },
-    messageNumberContainer: {
-        backgroundColor: "#f35bac",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "row",
-        width: 10,
-        height: 10,
-        borderRadius: 100,
-        position: "absolute",
-        right: 10,
-        top: 12
-    },
-    messageNumber: {
-        color: "white",
-        fontSize: 6,
-        fontFamily: getFontFamily("Inter", "600"),
-    },
-    userStoryContainer: {
-        marginTop: 20,
-        marginHorizontal: 28
-    },
-    userPostContainer: {
-        marginHorizontal: 24
-    }
-})
+	backgroundWhite: {
+		backgroundColor: 'white',
+	},
+	flex: {
+		flex: 1,
+	},
+	flexGrow: {
+		flexGrow: 1,
+	},
+});
 
-export default globalStyle
+export default globalStyle;
